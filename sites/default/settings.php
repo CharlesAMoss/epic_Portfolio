b49eec429c2a9893e8ae8bf2e2e9b1bfbaee2e9a
CharlesAMoss <charlesamoss@yahoo.com>
2015-10-19 20:09:35 -0700

    add drupal 7.4 for real this time

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -213,15 +213,15 @@
  * @endcode
  */
 $databases = array (
-  'default' => 
+  'default' =>
   array (
-    'default' => 
+    'default' =>
     array (
       'database' => 'charlesamoss',
       'username' => 'db_admin',
       'password' => 'fallinlove',
-      'host' => 'localhost',
-      'port' => '',
+      'host' => '127.0.0.1',
+      'port' => '8889',
       'driver' => 'mysql',
       'prefix' => '',
     ),
